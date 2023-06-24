3ca8ede52e6af282025a67f069d5ffa0880a4f3d
NickMisael <40251538+NickMisael@users.noreply.github.com>
2023-06-24 01:12:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -14,11 +14,10 @@ unsigned char **repBMP; // Representacao do BMP na memoria
 
 int main(int argc, char **argv){
 
-    //if(argc != 2)
-    //    usage(argv[0]);
+    if(argc != 2)
+        usage(argv[0]);
 
     FILE *fp;
-    char arg[] = "C:\\Users\\nicho\\projects\\curriculo_bootloader\\BitImage\\12.bmp";
     int totalWidth = 0, bytesPerRow = 0;
     int y, x;
     int actualByte = 0;
@@ -28,9 +27,7 @@ int main(int argc, char **argv){
     unsigned char bitAtual = 1; // Meu Run lenght comeca considerando preto como a cor
     unsigned char temp = 0;
 
-
-
-    if((fp = fopen(arg, "rb")) == NULL) {
+    if((fp = fopen(argv[1], "rb")) == NULL) {
         printf("Failed to open image!\n");
         exit(-1);
     }
@@ -79,45 +76,6 @@ int main(int argc, char **argv){
     for(y = bf.BIHeader.imageHeight-1; y >= 0; y--){
             printf("%s\n", repBMP[y]);
     }
-
-    /* BMP Compressor
-    totalWidth = bf.BIHeader.bitsPerPixel * bf.BIHeader.imageWidth;
-    bytesPerRow = ceilling(totalWidth, 32) * 4;
-    //repBMP = (unsigned char **) malloc(bf.BIHeader.imageHeight * sizeof(unsigned char *));
-
-    // Offset dos pixels da imagem
-    fseek(fp, bf.pixelDataOffset, SEEK_SET);
-
-    // Loop para ler e preencher na memoria com os Bits
-    for(y = 0; y < bf.BIHeader.imageHeight; y++){
-        //repBMP[y] = (unsigned char *) malloc(totalWidth * sizeof(unsigned char) + 1);
-        //memset(repBMP[y],'\0', (sizeof(unsigned char) * totalWidth + 1));
-        readBits = quantBitsSeguidos = 0;
-
-        for(x = 0; x < bytesPerRow; x++){
-            fread(&actualByte,1,1,fp);
-            for(count = 7; count >= 0; count--){
-                if(readBits < totalWidth) {
-                    if(count == 7 ) {
-                        bitAtual = checkBit(&actualByte, count);
-                        quantBitsSeguidos++;
-                    } else {
-                        if((temp = checkBit(&actualByte, count)) == bitAtual) quantBitsSeguidos++;
-                        else {
-                            printf("%d,", quantBitsSeguidos, temp);
-                            quantBitsSeguidos = 1;
-                            bitAtual = temp;
-                        }
-                        if(quantBitsSeguidos >= 0xff){
-                            return 0;
-                        }
-                    }
-                }
-                readBits++;
-            }
-        }
-        printf("%d,", quantBitsSeguidos);
-    }*/
 
     return 0;
 }
